6f3045b1707ec4263e1ed6d587f2e7ee6a7d8288
Oskar Agustsson <oskar.agustsson@gmail.com>
2021-07-05 15:04:06 +0000

    Update rsk.go
    Breytti ártalinu í 1700 - 2018
    
    Commentaði út þegar verið er að prenta þetta út í "console"

--- rsk.go
+++ rsk.go
@@ -13,13 +13,13 @@ func generateForYear(year int, c chan []string) {
 func main() {
 	c := make(chan []string)
 
-	for i := 1900; i < 2018; i++ {
+	for i := 1700; i < 2018; i++ {
 		go generateForYear(i, c)
 	}
 	total := 0
-	for i := 1900; i < 2018; i++ {
+	for i := 1700; i < 2018; i++ {
 		x := <-c
-		fmt.Printf("%d ids for year %d\n", len(x), i)
+	//	fmt.Printf("%d ids for year %d\n", len(x), i)
 		total += len(x)
 	}
 
